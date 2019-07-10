2111bc59cdafb75b0748e5c8059bd217ad2d6dc6
You Matsumoto <youmts22@gmail.com>
2019-07-10 23:30:13 +0900

    Add user name field to user registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'home/index'
 
-  devise_for :users, path: :devise
+  devise_for :users, path: :devise, controllers: { registrations: 'registrations' }
 
   root to: 'home#index'
 end
--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe "Users", :type => :system do
   it "サインアップできること" do
     visit new_user_registration_path
 
+    fill_in "名前", with: "れいるず　太郎"
     fill_in "メールアドレス", with: "test@example.com"
     fill_in "パスワード", with: "password"
     fill_in "確認用パスワード", with: "password"
@@ -11,6 +12,8 @@ RSpec.describe "Users", :type => :system do
     expect {
       click_button "登録"
     }.to change(User, :count).by(1)
+
+    expect(User.last.name).to eq "れいるず　太郎"
 
     expect(page).to have_current_path root_path
     expect(page).to have_content "アカウント登録が完了しました"
