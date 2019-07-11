d8b9cdd89048f0eb0dedcfd00d220d5025f0d472
You Matsumoto <youmts22@gmail.com>
2019-07-11 23:23:39 +0900

    Generate users_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'home/index'
+  root to: 'home#index'
+
+  resource :users, only: [:index, :show]
 
   devise_for :users, path: :devise, controllers: { registrations: 'registrations' }
-
-  root to: 'home#index'
 end
