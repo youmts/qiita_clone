d1ede9cabaddba447b81796a477da91b3c48051a
You Matsumoto <youmts22@gmail.com>
2019-07-11 23:22:04 +0900

    Add config.generators

--- config/application.rb
+++ config/application.rb
@@ -16,5 +16,18 @@ module QiitaClone
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.generators do |g|
+      g.assets false
+      g.skip_routes false
+      g.helper false
+      g.test_framework :rspec,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false,
+                       controller_specs: false,
+                       request_specs: false,
+                       fixtures: false
+    end
   end
 end
