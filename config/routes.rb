79714dae4113277916bfa2f3365a7c18d2f41582
You Matsumoto <youmts22@gmail.com>
2019-07-11 23:34:11 +0900

    Add users index/show

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'home/index'
   root to: 'home#index'
 
-  resource :users, only: [:index, :show]
+  resources :users, only: [:index, :show]
 
   devise_for :users, path: :devise, controllers: { registrations: 'registrations' }
 end
