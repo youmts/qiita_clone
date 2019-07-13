56039cedf3c9c068e949a97b2237eb98c3832957
You Matsumoto <youmts22@gmail.com>
2019-07-13 23:41:12 +0900

    Move articles resources to top level

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,25 +1,27 @@
 class ArticlesController < ApplicationController
-  before_action :set_user
-  before_action :authenticate_user!, only: [:new, :edit, :create, :update, :destroy]
-  before_action :current_user_is_owner!, only: [:new, :edit, :create, :update, :destroy]
+  before_action :authenticate_user!
   before_action :set_article, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @articles = current_user.articles
+  end
 
   def show
   end
 
   def new
-    @article = @user.articles.build
+    @article = current_user.articles.build
   end
 
   def edit
   end
 
   def create
-    @article = @user.articles.build(article_params)
+    @article = current_user.articles.build(article_params)
 
     respond_to do |format|
       if @article.save
-        format.html { redirect_to [@user, @article], notice: 'Article was successfully created.' }
+        format.html { redirect_to @article, notice: 'Article was successfully created.' }
       else
         format.html { render :new }
       end
@@ -29,7 +31,7 @@ class ArticlesController < ApplicationController
   def update
     respond_to do |format|
       if @article.update(article_params)
-        format.html { redirect_to [@user, @article], notice: 'Article was successfully updated.' }
+        format.html { redirect_to @article, notice: 'Article was successfully updated.' }
       else
         format.html { render :edit }
       end
@@ -39,21 +41,13 @@ class ArticlesController < ApplicationController
   def destroy
     @article.destroy
     respond_to do |format|
-      format.html { redirect_to @user, notice: 'Article was successfully destroyed.' }
+      format.html { redirect_to articles_path, notice: 'Article was successfully destroyed.' }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:user_id])
-    end
-    def current_user_is_owner!
-      # show 404 page
-      raise ActiveRecord::RecordNotFound if current_user != @user
-    end
     def set_article
-      @article = Article.find(params[:id])
+      @article = current_user.articles.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ Rails.application.routes.draw do
   get 'home/index'
   root to: 'home#index'
 
-  resources :users, only: [:index, :show] do
-    resources :articles
-  end
+  resources :users, only: [:index, :show]
+  resources :articles
 
   devise_for :users, path: :devise, controllers: { registrations: 'registrations' }
 end
