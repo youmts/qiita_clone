66899548224733ca695137a711ae4c3601ba05ec
You Matsumoto <youmts22@gmail.com>
2019-07-14 16:56:03 +0900

    Refine home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    @articles = Article.top(10)
   end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,6 @@ class Article < ApplicationRecord
   validates :body, presence: true
   
   belongs_to :user
+
+  scope :top, -> (count) { order(updated_at: :desc).limit(count) }
 end
