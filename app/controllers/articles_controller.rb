eeeb8ae037e7bc0475fed714cca0eb81015c6ea8
You Matsumoto <youmts22@gmail.com>
2019-07-15 10:23:47 +0900

    Add tag input for article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -54,6 +54,6 @@ class ArticlesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def article_params
-      params.require(:article).permit(:title, :body)
+      params.require(:article).permit(:title, :tag_list, :body)
     end
 end
