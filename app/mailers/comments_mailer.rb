21f9813137e0a8776a2d032aba6428a1cdde2777
You Matsumoto <youmts22@gmail.com>
2019-07-17 08:00:32 +0900

    Generate CommentsMailer

---
+++ app/mailers/comments_mailer.rb
@@ -0,0 +1,13 @@
+class CommentsMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.comments_mailer.create.subject
+  #
+  def create
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+end
