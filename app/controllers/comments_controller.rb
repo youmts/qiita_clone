1626a5928d5d4c5da28a196c988cce14145eae80
You Matsumoto <youmts22@gmail.com>
2019-07-16 17:08:26 +0900

    Add create comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,24 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_article
+
   def create
+    @comment = @article.comments.build(comment_params)
+
+    @comment.save!
   end
 
   def destroy
   end
+
+  private
+    def set_article
+      @article = Article.find(params[:article_id])
+    end
+
+    def comment_params
+      p = params.require(:comment).permit(:body)
+      p[:user] = current_user
+      p
+    end
 end
