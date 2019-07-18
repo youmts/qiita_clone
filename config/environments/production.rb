92bcc4b8dac9913281f31876ff05d31eeb5f6e5f
You Matsumoto <youmts22@gmail.com>
2019-07-18 21:46:37 +0900

    Make url https in mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: ENV['APP_HOST'] }
+  config.action_mailer.default_url_options = { host: ENV['APP_HOST'], protocol: :https }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
