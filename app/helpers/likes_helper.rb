5bfb74ed88c27dfe69d87a9fdc40ae4ef94cd505
You Matsumoto <youmts22@gmail.com>
2019-07-21 05:38:03 +0900

    Add icon for likes

--- app/helpers/likes_helper.rb
+++ app/helpers/likes_helper.rb
@@ -2,9 +2,11 @@ module LikesHelper
   def likes_link_to(article)
     return unless current_user
     if current_user.voted_up_on? article
-      return link_to 'Unlike', unlike_article_path(article), method: :delete, remote: true
+      return link_to content_tag(:i, "いいね済み", class: "fa fa-check"),
+                     unlike_article_path(article), method: :delete, remote: true
     else
-      return link_to 'Like', like_article_path(article), method: :post, remote: true
+      return link_to content_tag(:i, "いいね", class: "fa fa-thumbs-up"),
+                     like_article_path(article), method: :post, remote: true
     end
   end
 end
