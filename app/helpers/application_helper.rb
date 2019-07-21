225367b63fe00e5f3e3a3fd35e2447671bd929f3
You Matsumoto <youmts22@gmail.com>
2019-07-21 22:20:10 +0900

    Add anonymous support in article show

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def log_in_required(message, &block)
+    if current_user
+      capture(&block)
+    else
+      link_to message, new_user_session_url
+    end
+  end
 end
--- app/helpers/likes_helper.rb
+++ app/helpers/likes_helper.rb
@@ -1,6 +1,5 @@
 module LikesHelper
   def likes_link_to(article)
-    return unless current_user
     if current_user.voted_up_on? article
       return link_to content_tag(:i, "いいね済み", class: "fa fa-check"),
                      unlike_article_path(article), method: :delete, remote: true
