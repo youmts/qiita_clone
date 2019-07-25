037e04ff7dcd30dc5b0e2abcbd7a5f52193fa717
You Matsumoto <youmts22@gmail.com>
2019-07-25 20:25:31 +0900

    Remove unnecessary return

--- app/helpers/likes_helper.rb
+++ app/helpers/likes_helper.rb
@@ -1,10 +1,10 @@
 module LikesHelper
   def likes_link_to(article)
     if current_user.voted_up_on? article
-      return link_to content_tag(:i, "いいね済み", class: "fa fa-check"),
+      link_to content_tag(:i, "いいね済み", class: "fa fa-check"),
                      unlike_article_path(article), method: :delete, remote: true
     else
-      return link_to content_tag(:i, "いいね", class: "fa fa-thumbs-up"),
+      link_to content_tag(:i, "いいね", class: "fa fa-thumbs-up"),
                      like_article_path(article), method: :post, remote: true
     end
   end
