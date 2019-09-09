661d8acb52774e60e9470494cb3c1aad66f8895b
You Matsumoto <youmts22@gmail.com>
2019-09-09 20:52:21 +0900

    Refine design

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,6 +6,10 @@ class HomeController < ApplicationController
   end
 
   def tag_feed
-    @pagy, @articles = pagy(Article.tag_feed(current_user.tag_list).order(created_at: :desc))
+    if current_user
+      @pagy, @articles = pagy(Article.tag_feed(current_user.tag_list).order(created_at: :desc))
+    else
+      redirect_to root_path
+    end
   end
 end
--- spec/system/home_spec.rb
+++ spec/system/home_spec.rb
@@ -1,13 +1,31 @@
 require "rails_helper"
 
 RSpec.describe "Home", :type => :system do
-  example "トレンドが表示できること" do
-    visit root_path
-    expect(page).to have_current_path root_path
+  context "未ログインのとき" do
+    example "トレンドが表示できること" do
+      visit root_path
+      expect(page).to have_current_path root_path
+    end
+
+    example "タグフィードはリダイレクトすること" do
+      visit tag_feed_path
+      expect(page).to have_current_path root_path
+    end
   end
 
-  example "タグフィードが表示できること" do
-    visit tag_feed_path
-    expect(page).to have_current_path tag_feed_path
+  context "ログイン済みのとき" do
+    before do
+      sign_in user = create(:user)
+    end
+
+    example "トレンドが表示できること" do
+      visit root_path
+      expect(page).to have_current_path root_path
+    end
+
+    example "タグフィードが表示できること" do
+      visit tag_feed_path
+      expect(page).to have_current_path tag_feed_path
+    end
   end
 end
